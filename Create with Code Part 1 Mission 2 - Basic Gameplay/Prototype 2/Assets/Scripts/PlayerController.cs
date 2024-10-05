0fd2c5f22992746a3467f404408c63c4ee6dfae0
Sazib Bhuiyan <sazib.bhuiyan1@gmail.com>
2024-10-05 16:44:34 +0100

    Turn the three animals 180 degrees
    Animals are now facing the farmer.

--- Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerControler : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 10.0f;
 
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -29,5 +31,12 @@ public class PlayerControler : MonoBehaviour
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Launch a projectile from the player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+    
     }
 }
