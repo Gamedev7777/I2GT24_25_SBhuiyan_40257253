941061e02b50059590767b01dbe0400b6c27cf97
Sazib Bhuiyan <sazib.bhuiyan1@gmail.com>
2024-10-17 22:37:11 +0100

    Play the particle on collision

--- Prototype 3/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Prototype 3/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody _playerRb;
     private Animator _playerAnim;
+    public ParticleSystem explosionParticle;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -44,6 +45,7 @@ public class PlayerController : MonoBehaviour
             gameOver = true;
             _playerAnim.SetBool("Death_b", true);
             _playerAnim.SetInteger("DeathType_int",1);
+            explosionParticle.Play();
         }
     }
         
