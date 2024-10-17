9b507158bdc39993443148ab52caed59a093a4bb
Sazib Bhuiyan <sazib.bhuiyan1@gmail.com>
2024-10-17 20:59:31 +0100

    Prototype 3
    I forgot to make commits for Prototype 3 until now. So sorry.

---
+++ Prototype 3/Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float _speed = 30;
+    private PlayerController _playerControllerScript;
+
+    private float _leftBound = -15;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_playerControllerScript.gameOver == false)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * _speed); 
+        }
+
+        if (transform.position.x < _leftBound && gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+}
---
+++ Prototype 3/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _playerRb;
+    private Animator _playerAnim;
+    public float jumpForce = 10;
+    public float gravityModifier;
+    public bool isOnGround = true;
+    public bool gameOver;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+        _playerAnim = GetComponent<Animator>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+           _playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
+           isOnGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {       
+            Debug.Log("Game Over!");
+            gameOver = true;
+        }
+    }
+        
+}
