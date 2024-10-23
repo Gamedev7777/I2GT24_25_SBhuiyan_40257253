0d62d3d143b197fb0896ebd5d37e3108405f74ff
Sazib Bhuiyan <sazib.bhuiyan1@gmail.com>
2024-10-23 23:15:37 +0100

    Player movement and camera rotation coded

---
+++ Prototype 4/Prototype 4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody _playerRb;
+    private GameObject _focalPoint;
+    public float Speed = 5.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _playerRb = GetComponent<Rigidbody>();
+        _focalPoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        
+        _playerRb.AddForce(_focalPoint.transform.forward * forwardInput * Speed);
+    }
+}
