ee34f7b49dfb5bf2edb9ce94a97456d619135c5b
Sazib Bhuiyan <sazib.bhuiyan1@gmail.com>
2024-10-24 18:51:45 +0100

    Initialise score text and variable

--- Prototype 5/Prototype 5/Assets/Scripts/GameManager.cs
+++ Prototype 5/Prototype 5/Assets/Scripts/GameManager.cs
@@ -1,32 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+    public TextMeshProUGUI scoreText; 
+    private int _score;
     private float _spawnRate = 1.0f;
-        
+
     // Start is called before the first frame update
     void Start()
     {
         StartCoroutine(SpawnTarget());
+        _score = 0;
+        scoreText.text = "Score: " + _score;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    IEnumerator SpawnTarget()
+    IEnumerator SpawnTarget()  // Moved inside the class definition
     {
         while (true)
         {
             yield return new WaitForSeconds(_spawnRate);
             int index = Random.Range(0, targets.Count);
-            Instantiate(targets[index]);   
+            Instantiate(targets[index]);
         }
-        
     }
 }
