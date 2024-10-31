501f62358169e4531b9e4d5fa6437ba7b9a76896
Sazib Bhuiyan <sazib.bhuiyan1@gmail.com>
2024-10-31 16:49:45 +0000

    Testing laser firing at the player

--- Project/Personal Project/Assets/Scripts/AlienController.cs
+++ Project/Personal Project/Assets/Scripts/AlienController.cs
@@ -13,9 +13,12 @@ public class AlienController : MonoBehaviour
     private float _distanceToPlayer;
     private readonly float _stopDistance = 5.0f;
     public GameObject laserPrefab;
-    private float _laserSpeed = 10.0f;
+    private float _laserSpeed = 100.0f;
     private float _fireTimer;
     private float _fireInterval = 2.0f;
+    private Rigidbody _laserRb;
+    private Vector3 _laserDirection;
+    
 
     // Start is called before the first frame update
     void Start()
@@ -26,19 +29,41 @@ public class AlienController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    // Calculating distance between player and alien and saving it in the _distanceToPlayer variable
+        // Calculating distance between player and alien and saving it in the _distanceToPlayer variable
         _distanceToPlayer = Vector3.Distance(transform.position, targetPlayer.position);
-        
+
         // Checking if the alien is far enough from the player
-        
+
         if (_distanceToPlayer > _stopDistance)
         {
-        //Handles the movement of the alien following the player and the rotation of the alien to face the player
-            transform.position = Vector3.MoveTowards(transform.position, targetPlayer.position, _alienSpeed * Time.deltaTime);
+            //Handles the movement of the alien following the player and the rotation of the alien to face the player
+            transform.position =
+                Vector3.MoveTowards(transform.position, targetPlayer.position, _alienSpeed * Time.deltaTime);
             _direction = (targetPlayer.position - transform.position).normalized;
             _lookRotation = Quaternion.LookRotation(_direction);
             transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, _rotationSpeed * Time.deltaTime);
         }
+
         _fireTimer -= Time.deltaTime;
+        if (_fireTimer <= 0)
+        {
+            FireLaser();
+            _fireTimer = _fireInterval;
+        }
+        
+    }
+
+    void FireLaser()
+    {
+        if (laserPrefab != null)
+        {
+            _laserDirection = (targetPlayer.position - transform.position).normalized;
+           
+            Quaternion _laserRotation = Quaternion.LookRotation(_laserDirection);
+            GameObject laser = Instantiate(laserPrefab, transform.position, _laserRotation);
+            _laserRb = laser.GetComponent<Rigidbody>();
+            _laserRb.velocity = _laserDirection * _laserSpeed;
+        }
+        
     }
 }
