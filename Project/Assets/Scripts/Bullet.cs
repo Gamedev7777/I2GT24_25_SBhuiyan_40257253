d44d7d8a4e98f68955957d3873ba011c393045de
Sazib Bhuiyan <sazib.bhuiyan1@gmail.com>
2024-11-16 14:19:21 +0000

    Added gun and bullet models to both player avatars and added laser weapon to both aliens.

--- Project/Assets/Scripts/Bullet.cs
+++ Project/Assets/Scripts/Bullet.cs
@@ -1,37 +1,21 @@
+using System;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    // This method handles what happens when the bullet collides with various objects
-    void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision other)
     {
         // Checks if the bullet hits the alien
-        if (other.CompareTag("Alien1") || other.CompareTag("Alien2"))
+        if (other.gameObject.CompareTag("Alien1") || other.gameObject.CompareTag("Alien2"))
         {
             // Reduces the alien's health by one by calling the TakeDamage method in the AlienHealth component
             other.gameObject.GetComponent<AlienHealth>().TakeDamage(1);
-            // Destroys the bullet after it hits the alien to remove it from the scene
-            Destroy(gameObject);
-        }
-        // Checks if the bullet hits a fixed or moving wall
-        else if ((other.CompareTag("FixedWall")) || other.CompareTag("MovingWall"))
-        {
-            // Destroys the bullet when it hits a wall (either fixed or moving) to prevent it from passing through
-            Destroy(gameObject);
-        }
-        // Checks if the bullet hits a power-up of any type (speed, shield, or health)
-        else if ((other.CompareTag("PowerUpSpeed") || other.CompareTag("PowerUpShield")) ||
-                 other.CompareTag("PowerUpHealth"))
-        {
-            // Destroys the bullet if it hits any type of power-up to prevent unintended interactions
             Destroy(gameObject);
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        // Destroys the bullet after 1 second to prevent cluttering the scene and keeps the game optimised
-        Destroy(gameObject, 1.0f);
+        Destroy(gameObject, 0.5f);
     }
 }
