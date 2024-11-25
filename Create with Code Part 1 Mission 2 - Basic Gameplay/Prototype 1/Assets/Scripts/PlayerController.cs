e982a370dd9cb9c8231aace1341633962e81689a
Sazib Bhuiyan <sazib.bhuiyan1@gmail.com>
2024-11-25 20:05:23 +0000

    Prevented driving in mid-air in Prototype 1 for lesson 6.2

--- Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code Part 1 Mission 2 - Basic Gameplay/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
@@ -10,6 +11,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private TextMeshProUGUI speedometerText;
     [SerializeField] private TextMeshProUGUI rpmText;
     [SerializeField] private float rpm;
+    [SerializeField] List<WheelCollider> allWheels;
+    [SerializeField] private int wheelsOnGround;
     private const float _turnSpeed = 45.0f;
     private float _horizontalInput;
     private float _verticalInput;
@@ -31,12 +34,36 @@ public class PlayerController : MonoBehaviour
 
         // Move the vehicle forward or backward
         //transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-        _playerRb.AddRelativeForce(Vector3.forward * _verticalInput * horsePower);
-        // Turn the vehicle left or right
-        transform.Rotate(Vector3.up, _turnSpeed * _horizontalInput * Time.deltaTime);
-        speed = Mathf.RoundToInt(_playerRb.velocity.magnitude * 2.237f); // This shows speed in miles per hour
-        speedometerText.SetText("Speed: " + speed + "mph");
-        rpm = Mathf.RoundToInt((speed % 30) * 40);
-        rpmText.SetText("RPM: " + rpm);
+        if (IsOnGround())
+        {
+            _playerRb.AddRelativeForce(Vector3.forward * _verticalInput * horsePower);
+            // Turn the vehicle left or right
+            transform.Rotate(Vector3.up, _turnSpeed * _horizontalInput * Time.deltaTime);
+            speed = Mathf.RoundToInt(_playerRb.velocity.magnitude * 2.237f); // This shows speed in miles per hour
+            speedometerText.SetText("Speed: " + speed + "mph");
+            rpm = Mathf.RoundToInt((speed % 30) * 40);
+            rpmText.SetText("RPM: " + rpm);
+        }
+    }
+
+    bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
