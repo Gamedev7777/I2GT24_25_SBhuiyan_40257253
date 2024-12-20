38a35a6de3a89fc518673b98aeaf95401b2765f0
Sazib Bhuiyan <sazib.bhuiyan1@gmail.com>
2024-12-20 10:52:35 +0000

    Finished refactoring Spawn Manager script.

--- Project/Assets/Scripts/SpawnManager.cs
+++ Project/Assets/Scripts/SpawnManager.cs
@@ -5,7 +5,7 @@ using UnityEngine.SceneManagement;
 public class SpawnManager : MonoBehaviour
 {
     // public variables
-    public static SpawnManager instance; // Singleton instance of SpawnManager for easy access
+    public static SpawnManager instance; // Instance of SpawnManager for easy access
     public List<GameObject> alienList = new(); // List to keep track of all spawned aliens
     public GameObject levelSpawned; // Reference to the currently spawned level
     public List<GameObject> levelPrefabList = new(); // List of level prefabs for single-player mode
@@ -16,7 +16,7 @@ public class SpawnManager : MonoBehaviour
     // Awake is called when the script instance is being loaded
     private void Awake()
     {
-        // Sets up the singleton instance
+        // Initialises the instance
         instance = this;
     }
 
@@ -48,6 +48,7 @@ public class SpawnManager : MonoBehaviour
 
     private void AddAliensToList()
     {
+        // All the aliens that spawn are being added to the list of aliens in the SpawnManager script
         for (int i = 0; i < levelSpawned.GetComponent<Levels>().aliens.Count; i++)
         {
             // Adds the newly spawned alien to the alien list
@@ -65,14 +66,7 @@ public class SpawnManager : MonoBehaviour
     private void SetupCutscene()
     {
         // Setting the value for the cutscene PlayerPrefs and use it to play video before level 1 and cutscenes for other levels
-        if (PlayerPrefs.GetInt("Level", 1) == 1)
-        {
-            PlayerPrefs.SetInt("Cutscene", 0);
-        }
-        else
-        {
-            PlayerPrefs.SetInt("Cutscene", 1);
-        }
+        PlayerPrefs.SetInt("Cutscene", PlayerPrefs.GetInt("Level", 1) == 1 ? 0 : 1);
 
         fakeCamera.gameObject.SetActive(false); // Deactivate the fake camera
     }
@@ -80,55 +74,54 @@ public class SpawnManager : MonoBehaviour
     // Called when the level is completed
     public void LevelComplete()
     {
-        // Checks if all aliens have been eliminated
-        if (alienList.Count <= 0)
+        // Checks if there is any alien still alive
+        if (alienList.Count > 0) return;
+        // If the player has completed all 7 levels, shows the final pop-up and reset the level
+        if (PlayerPrefs.GetInt("Level", 1) == 7)
         {
-            
-            // If the player has completed all 7 levels, shows the final pop-up and reset the level
-            if (PlayerPrefs.GetInt("Level", 1) == 7)
-            {
-                // Checks if the player has achieved a new high score
-                if (PlayerPrefs.GetInt("Highscore", 0) < PlayerPrefs.GetInt("Score", 0))
-                {
-                    Cursor.lockState = CursorLockMode.None; // Lock the cursor to the center of the game window.
-                    Cursor.visible = true;
-                    levelSpawned.SetActive(false);
-                    GameManager.instance.videoCamera.SetActive(true);
-                    GameManager.instance.popUpList[7].SetActive(true); // Shows the upgrade story pop-up
-                    
-                    if (PlayerPrefs.GetInt("Avatar", 0) == 0)
-                    {
-                        // Remy is chosen
-                        GameManager.instance.videoPlayer8.clip = GameManager.instance.remyVideoClip8;
-
-                    }
-                    else if (PlayerPrefs.GetInt("Avatar", 0) == 1)
-                    {
-                        // Claire is chosen
-                        GameManager.instance.videoPlayer8.clip = GameManager.instance.claireVideoClip8;
-
-                    }
-                    
-                    GameManager.instance.videoPlayer8.gameObject.SetActive(true);
-                }
-                else
-                {
-                    Cursor.lockState = CursorLockMode.None; // Unlock the cursor to the center of the game window.
-                    Cursor.visible = true;
-                    GameManager.instance.popUpList[7].SetActive(true); // Shows the final level completion pop-up
-                }
-
-                PlayerPrefs.SetInt("Level", 1); // Resets level to 1
-                PlayerPrefs.SetInt("firstTime", 0); // Marks that the first-time setup is complete
-                PlayerPrefs.SetInt("Upgraded", 0); // Resets upgrade state
-            }
-            else
-            {
-                // Increments the level and loads the next level
-                PlayerPrefs.SetInt("Level", PlayerPrefs.GetInt("Level", 1) + 1); // Advances to the next level
-                Invoke(nameof(LevelLoad), 1.0f); // Loads the next level after a delay of 1 second
-            }
+            UnlockCursor();
+            levelSpawned.SetActive(false);
+            PlayPostLevel7Video();
+            ResetPlayerPrefs();
         }
+        else
+        {
+            // Increments the level and loads the next level
+            PlayerPrefs.SetInt("Level", PlayerPrefs.GetInt("Level", 1) + 1); // Advances to the next level
+            Invoke(nameof(LevelLoad), 1.0f); // Loads the next level after a delay of 1 second
+        }
+    }
+
+    private static void ResetPlayerPrefs()
+    {
+        PlayerPrefs.SetInt("Level", 1); // Resets level to 1
+        PlayerPrefs.SetInt("firstTime", 0); // Marks that the first-time setup is complete
+        PlayerPrefs.SetInt("Upgraded", 0); // Resets upgrade state
+    }
+
+    private static void PlayPostLevel7Video()
+    {
+        GameManager.instance.videoCamera.SetActive(true); // Turns on the video camera to show video
+        GameManager.instance.popUpList[7].SetActive(true); // Shows the upgrade story pop-up
+                    
+        if (PlayerPrefs.GetInt("Avatar", 0) == 0)
+        {
+            // Remy is chosen
+            GameManager.instance.videoPlayer8.clip = GameManager.instance.remyVideoClip8;
+        }
+        else if (PlayerPrefs.GetInt("Avatar", 0) == 1)
+        {
+            // Claire is chosen
+            GameManager.instance.videoPlayer8.clip = GameManager.instance.claireVideoClip8;
+        }
+        
+        GameManager.instance.videoPlayer8.gameObject.SetActive(true);
+    }
+
+    private static void UnlockCursor()
+    {
+        Cursor.lockState = CursorLockMode.None; // Unlock the cursor
+        Cursor.visible = true;
     }
 
     // Called when the game is over
@@ -149,7 +142,7 @@ public class SpawnManager : MonoBehaviour
     }
 
     // Processes and assigns a player as the target for each alien in multiplayer mode
-    void ProcessAlienTarget()
+    private void ProcessAlienTarget()
     {
         var aliens = FindObjectsOfType<AlienAI>(); // Finds all AlienAI instances
 
@@ -160,7 +153,7 @@ public class SpawnManager : MonoBehaviour
     }
 
     // Loads the current level scene
-    void LevelLoad()
+    private void LevelLoad()
     {
         SceneManager.LoadScene("Adapt or Die"); // Loads the scene named "Adapt or Die"
     }
@@ -172,56 +165,76 @@ public class SpawnManager : MonoBehaviour
     }
 
     // Disables the player's shield and updates the User Interface
-    void DisablePlayerShield()
+    private void DisablePlayerShield()
     {
-        AudioSource.PlayClipAtPoint(shieldDisabledSound, transform.position); // Plays sound when the shield is disabled
+        PlayShieldDisabledSound();
         // Checks if the game is in single-player mode
         if (PlayerPrefs.GetInt("Controller", 0) == 0)
         {
-            // Disables the shield for player 1
-            levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerHealth>().playerShield = false;
-
-            levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerController>().remyShield.SetActive(false);
-
-            levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerController>().claireShield
-                .SetActive(false);
+            DisableShieldSingleplayerMode();
         }
         // Checks if the game is in multiplayer mode
         else if (PlayerPrefs.GetInt("Controller", 0) == 1)
         {
             if (levelSpawned.GetComponent<Levels>().player.Count > 1)
             {
-                // Disables the shield for both players
-                levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerHealth>().playerShield = false;
-                levelSpawned.GetComponent<Levels>().player[1].GetComponent<PlayerHealth>().playerShield = false;
-
-                levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerMultiplayer1>().remyShield
-                    .SetActive(false);
-
-                levelSpawned.GetComponent<Levels>().player[1].GetComponent<PlayerMultiplayer2>().claireShield
-                    .SetActive(false);
+                DisableShieldsForBothPlayersMultiplayerMode();
             }
             else
             {
-                if (levelSpawned.GetComponent<Levels>().player[0].name == "PlayerMultiplayer1")
-                {
-                    // Disables the shield for both players
-                    levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerHealth>().playerShield = false;
-
-
-                    levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerMultiplayer1>().remyShield
-                        .SetActive(false);
-                }
-                else if (levelSpawned.GetComponent<Levels>().player[0].name == "PlayerMultiplayer2")
-                {
-                    levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerHealth>().playerShield = false;
-                    levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerMultiplayer2>().claireShield
-                        .SetActive(false);
-                }
+                DisableShieldForAlivePlayerMultiplayerMode();
             }
         }
 
         // Hides the shield User Interface text
         GameManager.instance.playerShieldText.SetActive(false);
+    }
+
+    private void DisableShieldForAlivePlayerMultiplayerMode()
+    {
+        if (levelSpawned.GetComponent<Levels>().player[0].name == "PlayerMultiplayer1")
+        {
+            // Disables the shield for both players
+            levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerHealth>().playerShield = false;
+
+
+            levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerMultiplayer1>().remyShield
+                .SetActive(false);
+        }
+        else if (levelSpawned.GetComponent<Levels>().player[0].name == "PlayerMultiplayer2")
+        {
+            levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerHealth>().playerShield = false;
+            levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerMultiplayer2>().claireShield
+                .SetActive(false);
+        }
+    }
+
+    private void DisableShieldsForBothPlayersMultiplayerMode()
+    {
+        // Disables the shield for both players
+        levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerHealth>().playerShield = false;
+        levelSpawned.GetComponent<Levels>().player[1].GetComponent<PlayerHealth>().playerShield = false;
+
+        levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerMultiplayer1>().remyShield
+            .SetActive(false);
+
+        levelSpawned.GetComponent<Levels>().player[1].GetComponent<PlayerMultiplayer2>().claireShield
+            .SetActive(false);
+    }
+
+    private void DisableShieldSingleplayerMode()
+    {
+        // Disables the shield for player 1
+        levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerHealth>().playerShield = false;
+
+        levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerController>().remyShield.SetActive(false);
+
+        levelSpawned.GetComponent<Levels>().player[0].GetComponent<PlayerController>().claireShield
+            .SetActive(false);
+    }
+
+    private void PlayShieldDisabledSound()
+    {
+        AudioSource.PlayClipAtPoint(shieldDisabledSound, transform.position); // Plays sound when the shield is disabled
     }
 }
